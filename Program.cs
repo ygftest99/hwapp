5d50437b0f6678a69ea7c7d1de6d8ab5567f81d4
ygftest99 <guofayang@guofa.local>
2016-05-26 23:03:40 +0800

    shuru

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication
            int i=100222;
            
            i++;
-           
+           i=i*98;
             Console.WriteLine("i="+i);
            
             Console.WriteLine("Hello  222222 111111111  Git  2222  test  World!");
